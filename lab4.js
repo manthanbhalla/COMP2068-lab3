ed679dc5add98b2210a8cd3ef69a3af220f813ac
manthanbhalla <manthan.bhalla4@gmail.com>
2017-01-26 16:46:13 -0500

    first commit

--- lab4.js
+++ lab4.js
@@ -1,155 +1,82 @@
-var connect
-=
-require('connect');
+var connect = require('connect');
 	
 	
 //link to the url module
 	
 	
-var url
-=
-require('url');
+var url = require('url');
 	
 	
 //create a new app using connect
 	
-var app
-=
-connect();
+var app = connect();
 	
 	
 	
-var
-calculate
-=
-function (req,
-res, 
-next) {
+var calculate = function (req,res,next) {
 	
 // get calculator from the url querystring
 	
-var qs
-=
-url.parse(req.url,
-true).query;
+var qs = url.parse(req.url,true).query;
 	
-var method
-=
-qs.method;
+var method = qs.method;
 	
-var x
-=
-qs.x;
+var x = qs.x;
 	
-var y
-=
-qs.y;
+var y = qs.y;
 	
 var total;
 	
 var symbol;
 	
 	
-if (method
-===
-'add') {
+if (method === 'add') {
 	
-total 
-= 
-parseFloat(x) 
-+ 
-parseFloat(y);
+total =  parseFloat(x) + parseFloat(y);
 	
-symbol 
-= 
-'+';
+symbol = '+';
 	
-} else
-if (method
-===
-'subtract') {
+} 
+else if (method === 'subtract') {
 	
 	
-total 
-= 
-parseFloat(x) 
-- 
-parseFloat(y);
+total = parseFloat(x) - parseFloat(y);
 	
-symbol 
-= 
-'-';
+symbol  =  '-';
 	
-} else
-if (method
-===
-'multiply') {
+} 
+else if (method === 'multiply') {
+		
+total = parseFloat(x) * parseFloat(y);
 	
+symbol = '*';
 	
-total 
-= 
-parseFloat(x) 
-* 
-parseFloat(y);
-	
-symbol 
-= 
-'*';
-	
-} else
-if (method
-===
-'divide') {
+} 
+else if (method === 'divide') {
 	
 	
-total 
-= 
-parseFloat(x) 
-/ 
-parseFloat(y);
+total = parseFloat(x) / parseFloat(y);
 	
-symbol 
-= 
-'/';
+symbol = '/';
 	
-} else {
+}
+else {
 	
-return
-"Not Valid";
-	
-	
+return "Not Valid";	
 }
 	
 	
 res.writeHead(200, {
 	
 	
-"Content-Type":
-"text-plain"
-	
-	
+"Content-Type": "text-plain"
+		
 });
 	
 	
 //display result
 	
-res.write('Output:
-' 
-+ x 
-+ 
-' 
-' 
-+ symbol 
-+ 
-' 
-' 
-+ y 
-+ 
-' = 
-' 
-+ total 
-+ 
-'\n');
+res.write('Output:' + x + ' ' + symbol + ' ' + y + ' = ' + total + '\n');
 	
 res.end()
 	
@@ -160,13 +87,11 @@ res.end()
 // route each url to proper function
 	
 	
-app.use('/lab3',
- calculate);
+app.use('/lab3',calculate);
 	
 	
 //listen to the events
 	
 app.listen(3000);
 	
-console.log('Connect
- app running at http:// localhost:3000');
+console.log('Connect app running at http:// localhost:3000');
